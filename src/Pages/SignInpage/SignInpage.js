f9df8070d3dad01708f45fb642cbb0ea7ebd6492
Just-A-Pixel <Raj.Anand0511@gmail.com>
2021-03-20 12:38:43 +0530

    sign in

--- src/Pages/SignInpage/SignInpage.js
+++ src/Pages/SignInpage/SignInpage.js
@@ -3,14 +3,20 @@ import { motion } from "framer-motion"
 import "./SignIn.scss";
 
 const SignInPage = ()=> {
+    // const [signin, setSignin] = useState(true);
+    const [move, setMove] = useState(0);
+    // useEffect(()=>{
+    console.log(move)
+    // }, [signin]) 
     return(
         <div className="log-in">
-            <motion.div animate={{x:0}} className="back-box">
-                awdaw
+            <motion.div className="back-box">
+                <span   onClick={()=>{setMove(0)}}>Sign Up</span>
+                <span style={{marginLeft:'800px'}} onClick={()=>{setMove(520)}}>Log in</span>
             </motion.div>
 
-            <motion.div animate={{x:0}} className="front-box">
-                awdaw
+            <motion.div animate={{x:move, }}  className="front-box">
+                
             </motion.div>
     </div>
     )

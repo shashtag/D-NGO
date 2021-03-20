031272959515d79cf926d7e5503c915133c50f80
Just-A-Pixel <Raj.Anand0511@gmail.com>
2021-03-20 11:56:56 +0530

    sign in

---
+++ src/Pages/SignInpage/SignInpage.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import { motion } from "framer-motion"
+import "./SignIn.scss";
+
+const SignInPage = ()=> {
+    return(
+        <div className="log-in">
+            <motion.div animate={{x:0}} className="back-box">
+                awdaw
+            </motion.div>
+
+            <motion.div animate={{x:0}} className="front-box">
+                awdaw
+            </motion.div>
+    </div>
+    )
+}
+
+export default SignInPage;
